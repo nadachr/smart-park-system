4d781986deabe63d8f3b86512647b8ade019fe92
nadachr <nnada.chr@gmail.com>
2020-11-19 11:22:32 +0700

    90%

--- index.php
+++ index.php
@@ -89,14 +89,13 @@
       <nav class="navbar navbar-expand-lg">
         <div class="container-fluid d-flex align-items-center justify-content-between">
           <div class="navbar-header">
-            <!-- Navbar Header--><a href="index.html" class="navbar-brand">
+            <a href="index.php" class="navbar-brand">
               <div class="brand-text brand-big visible text-uppercase"><strong>Smart</strong><strong class="text-primary">Park</strong></div>
-              <div class="brand-text brand-sm"><strong>S</strong><strong class="text-primary">P</strong></div></a>
-            <!-- Sidebar Toggle Btn-->
+              <div class="brand-text brand-sm"><strong>S</strong><strong class="text-primary">P</strong></div>
+            </a>
             <button class="sidebar-toggle"><i class="fa fa-long-arrow-left"></i></button>
           </div>
           <div class="right-menu list-inline no-margin-bottom">    
-            <!-- Log out -->
             <div class="list-inline-item logout">
               <a id="logout" href="logout.php" class="nav-link">
                 <span class="d-none d-sm-inline">Logout </span>
@@ -108,9 +107,7 @@
       </nav>
     </header>
     <div class="d-flex align-items-stretch">
-      <!-- Sidebar Navigation-->
       <nav id="sidebar">
-        <!-- Sidebar Header-->
         <div class="sidebar-header d-flex align-items-center">
           <div class="avatar"><img src="img/avatar.svg" alt="..." class="img-fluid rounded-circle"></div>
           <div class="title">
@@ -118,7 +115,7 @@
             <p>Administrator</p>
           </div>
         </div>
-        <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
+        <span class="heading">Main</span>
         <ul class="list-unstyled">
           <li class="active"><a href="index.php"> <i class="icon-writing-whiteboard"></i>Dashboard </a></li>
           <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-user"></i>Member </a>
@@ -131,7 +128,6 @@
           </li>
         </ul>
       </nav>
-      <!-- Sidebar Navigation end-->
       <div class="page-content">
         <div class="page-header">
           <div class="container-fluid">
@@ -261,172 +257,8 @@
     <script src="vendor/popper.js/umd/popper.min.js"> </script>
     <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
     <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
-    <script src="vendor/chart.js/Chart.min.js"></script>
     <script src="vendor/jquery-validation/jquery.validate.min.js"></script>
     <script src="js/config.js"></script>
     <script src="js/front.js"></script>
-    <!-- <script>
-      /*global $, document*/
-      window.onload = function () {
-          var BARCHARTEXMPLE    = $('#barChart');
-          window.chartSensor = new Chart(BARCHARTEXMPLE,sensor);
-      };
-
-      var sensor = {
-          type: 'bar',
-          options: {
-              scales: {
-                  xAxes: [{
-                      display: true,
-                      gridLines: {
-                          color: 'transparent'
-                      }
-                  }],
-                  yAxes: [{
-                      display: true,
-                      gridLines: {
-                          color: 'transparent'
-                      }
-                  }]
-              },
-          },
-          data: {
-              labels: ["novber","November"],
-              datasets: [
-                  {
-                      label: "People",
-                      backgroundColor: [
-                          "#fffb00",
-                          "#fffb00",
-                          "#fffb00",
-                          "#fffb00",
-                          "#fffb00",
-                          "#fffb00",
-                          "#fffb00"
-                      ],
-                      hoverBackgroundColor: [
-                          "#fffb00",
-                          "#fffb00",
-                          "#fffb00",
-                          "#fffb00",
-                          "#fffb00",
-                          "#fffb00",
-                          "#fffb00"
-                      ],
-                      borderColor: [
-                          "#fffb00",
-                          "#fffb00",
-                          "#fffb00",
-                          "#fffb00",
-                          "#fffb00",
-                          "#fffb00",
-                          "#fffb00"
-                      ],
-                      borderWidth: 0.5,
-                      data: [],
-                  },
-                  {
-                      label: "Vehicle",
-                      backgroundColor: [
-                          "#00ff88",
-                          "#00ff88",
-                          "#00ff88",
-                          "#00ff88",
-                          "#00ff88",
-                          "#00ff88",
-                          "#00ff88"
-                      ],
-                      hoverBackgroundColor: [
-                          "#00ff88",
-                          "#00ff88",
-                          "#00ff88",
-                          "#00ff88",
-                          "#00ff88",
-                          "#00ff88",
-                          "#00ff88"
-                      ],
-                      borderColor: [
-                          "#00ff88",
-                          "#00ff88",
-                          "#00ff88",
-                          "#00ff88",
-                          "#00ff88",
-                          "#00ff88",
-                          "#00ff88"
-                      ],
-                      borderWidth: 0.5,
-                      data: [],
-                  }
-              ]
-          }
-      };
-
-      var firebaseConfig = {
-          apiKey: "AIzaSyBBYQjQRwalw7afWl4NkgDoKNidcp3QSLA",
-          authDomain: "peoplencardetect.firebaseapp.com",
-          databaseURL: "https://peoplencardetect.firebaseio.com",
-          projectId: "peoplencardetect",
-          storageBucket: "peoplencardetect.appspot.com",
-          messagingSenderId: "155222711004",
-          appId: "1:155222711004:web:74ce98d10df5a333133219",
-          measurementId: "G-26VXKPN7YJ"
-      };
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
-      firebase.analytics();
-      var database = firebase.database();
-      var people = document.getElementById('pp');
-      var car = document.getElementById('car');
-      //var datenow = document.getElementById('datenow');
-      var date = new Date();
-      var d = date.getDate();
-      var m = date.getMonth() + 1;
-      var y = date.getFullYear();
-      var now = y + "-" + m + "-" + d;
-      var valp = 0;
-      var valc = 0;
-      var disp = false;
-
-
-      setInterval(function(){
-          database.ref("pnc").once('value').then(function(snapshot){
-              if(snapshot.exists()){
-                  var pc = snapshot.val();
-                  for(var key of Object.keys(pc)){
-                      var pc2 = pc[key];
-
-                      for(var key2 of Object.keys(pc2)){
-                          if(key2 == "date"){
-                              var day = pc2[key2];
-                              if(now == day){
-                                  disp = true;
-                              }
-                              else{
-                                  disp = false;
-                              }
-                          }
-                          if(disp == true){
-                              if(key2 == "Total_person"){
-                                  valp = pc2[key2];
-                                  people.innerHTML = valp;
-                              }
-                              else if(key2 == "Total_car"){
-                                  valc = pc2[key2];
-                                  car.innerHTML = valc;
-                              }
-                          }
-                      }window.chartSensor.update();
-                  } 
-              }  
-              sensor.data.datasets[0].data.push(valp);
-              sensor.data.datasets[1].data.push(valc);     
-
-              
-              console.log("now: ",now);
-              console.log("date: ", day);       
-          })
-      }, 1000);
-    </script> -->
-
   </body>
 </html>
